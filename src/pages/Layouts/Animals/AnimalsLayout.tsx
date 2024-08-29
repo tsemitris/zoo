d40de9ebbceaea0567014d60d718b48ec545355f
tsemitris <94851943+tsemitris@users.noreply.github.com>
2024-08-29 22:09:24 +0200

    :art: Add animalsLayout.tsx and applied styling

---
+++ src/pages/Layouts/Animals/AnimalsLayout.tsx
@@ -0,0 +1,44 @@
+import { Link, Outlet, useLocation } from "react-router-dom";
+
+export const AnimalsLayout = () => {
+  const location = useLocation();
+
+  const isActive = (path: string) =>
+    location.pathname === path ? "active" : "";
+
+  return (
+    <>
+      <section className="video-container">
+        <div className="animals-header">
+          <p className="animals-title">Möt våra djurvänner!</p>
+          <div className="animals-nav">
+            <Link
+              className={`nav-option ${isActive("/animals")}`}
+              to="/animals"
+            >
+              Alla
+            </Link>
+            <Link
+              className={`nav-option ${isActive("/animals/fed")}`}
+              to="/animals/fed"
+            >
+              Matade djur
+            </Link>
+            <Link
+              className={`nav-option ${isActive("/animals/unfed")}`}
+              to="/animals/unfed"
+            >
+              Omatade djur
+            </Link>
+          </div>
+        </div>
+        <video autoPlay playsInline>
+          <source src="/videos/zooPenguins.mp4" type="video/mp4" />
+        </video>
+      </section>
+      <section className="animals-container">
+        <Outlet></Outlet>
+      </section>
+    </>
+  );
+};
