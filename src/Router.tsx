a5981ccf6191f72bd05cb67903dc69977e8b03af
tsemitris <94851943+tsemitris@users.noreply.github.com>
2024-08-29 22:13:20 +0200

    :construction:  Add new routes and layout for animals pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Layout } from "./pages/Layout/layout";
+import { Layout } from "./pages/Layouts/Website/layout";
 import { Home } from "./pages/home";
 import { Animals } from "./pages/Animals/animals";
 import { animalLoader } from "./loaders/animalLoader";
 import { Animal } from "./pages/Animal/animal";
+import { fedAnimalsStatusLoader } from "./loaders/fedAnimalsStatusLoader";
+import { FedAnimals } from "./pages/FedAnimals/FedAnimals";
+import { AnimalsLayout } from "./pages/Layouts/Animals/AnimalsLayout";
+import { UnfedAnimals } from "./pages/UnfedAnimals/UnfedAnimals";
 
 export const router = createBrowserRouter([
   {
@@ -16,7 +20,23 @@ export const router = createBrowserRouter([
       },
       {
         path: "/animals",
-        element: <Animals></Animals>,
+        element: <AnimalsLayout></AnimalsLayout>,
+        children: [
+          {
+            path: "",
+            element: <Animals></Animals>,
+          },
+          {
+            path: "fed",
+            element: <FedAnimals></FedAnimals>,
+            loader: () => fedAnimalsStatusLoader(true),
+          },
+          {
+            path: "unfed",
+            element: <UnfedAnimals></UnfedAnimals>,
+            loader: () => fedAnimalsStatusLoader(false),
+          },
+        ],
       },
       {
         path: "/animal/:id",
