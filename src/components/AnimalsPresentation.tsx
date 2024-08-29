1877352de08fc733883fc1da9efe5e823e511dca
tsemitris <94851943+tsemitris@users.noreply.github.com>
2024-08-29 22:11:00 +0200

    :recycle: Remove handleImageNotFound / Add Fed / Unfed feature

--- src/components/AnimalsPresentation.tsx
+++ src/components/AnimalsPresentation.tsx
@@ -1,45 +1,48 @@
 import { Link } from "react-router-dom";
-import { IAnimal } from "../models/IAnimal";
+import { handleImageNotFound } from "../utils/imageHandlers";
+import { IAnimalExt } from "../models/IAnimalExt";
 
 interface IAnimalsPresentationProps {
-  animals: IAnimal[];
+  animals: IAnimalExt[];
 }
 
 export const AnimalsPresentation = ({ animals }: IAnimalsPresentationProps) => {
-  const handleImageNotFound = (
-    e: React.SyntheticEvent<HTMLImageElement, Event>
-  ) => {
-    e.currentTarget.src = "/image-not-found.jpg";
-  };
-
   return (
     <>
-      <section className="animals-container">
-        {animals.map((animal) => (
-          <div className="animal-card" key={animal.id}>
-            <div className="animal-profile">
-              <img
-                className="animal-image"
-                src={animal.imageUrl}
-                alt={animal.name}
-                loading="lazy"
-                onError={handleImageNotFound}
-              />
-              <p className="animal-name">{animal.name}</p>
-            </div>
-            <p className="animal-short-desc">{animal.shortDescription}</p>
-            <div className="animal-button-options">
-              <Link to={`/animal/${animal.id}`} className="animal-read-more">
-                Läs mer
-              </Link>
-              <div className="animal-fed-status">
-                <span className="status-indicator"></span>
-                Matad
-              </div>
-            </div>
+      {animals.map((animal) => (
+        <div className="animal-card" key={animal.id}>
+          <div className="animal-profile">
+            <img
+              className="animal-image"
+              src={animal.imageUrl}
+              alt={animal.name}
+              loading="lazy"
+              onError={handleImageNotFound}
+            />
+            <p className="animal-name">{animal.name}</p>
           </div>
-        ))}
-      </section>
+          <p className="animal-short-desc">{animal.shortDescription}</p>
+          <div className="animal-button-options">
+            <div className="animal-fed-status">
+              {animal.isFed ? (
+                <>
+                  <span className="status-indicator status-full"></span>
+                  Matad
+                </>
+              ) : (
+                <>
+                  <span className="status-indicator status-hungry"></span>
+                  Omatad
+                </>
+              )}
+            </div>
+
+            <Link to={`/animal/${animal.id}`} className="global-btn">
+              Läs mer
+            </Link>
+          </div>
+        </div>
+      ))}
     </>
   );
 };
