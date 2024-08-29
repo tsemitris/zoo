49e642c5f4a5c3105afb4bce23a37339f0fd1a82
tsemitris <94851943+tsemitris@users.noreply.github.com>
2024-08-29 22:03:18 +0200

    :sparkles: Add fed- / unfedAnimlas.tsx as well as fedAnimals.scss

---
+++ src/pages/FedAnimals/FedAnimals.tsx
@@ -0,0 +1,26 @@
+import { useLoaderData } from "react-router-dom";
+import { IAnimalExt } from "../../models/IAnimalExt";
+import { AnimalsPresentation } from "../../components/AnimalsPresentation";
+
+export const FedAnimals = () => {
+  const fedAnimals = useLoaderData() as IAnimalExt[] | null;
+
+  if (!fedAnimals) {
+    return (
+      <>
+        <section className="message-contaienr">
+          <img
+            className="avatar"
+            src="/images/sadAnimal.svg"
+            alt="A sad dog with orange shirt and pants"
+          />
+          <p className="message">Inga matade djur hittades!</p>
+        </section>
+      </>
+    );
+  }
+
+  return (
+    <>{<AnimalsPresentation animals={fedAnimals}></AnimalsPresentation>}</>
+  );
+};
---
+++ src/pages/UnfedAnimals/UnfedAnimals.tsx
@@ -0,0 +1,26 @@
+import { useLoaderData } from "react-router-dom";
+import { IAnimalExt } from "../../models/IAnimalExt";
+import { AnimalsPresentation } from "../../components/AnimalsPresentation";
+
+export const UnfedAnimals = () => {
+  const fedAnimals = useLoaderData() as IAnimalExt[] | null;
+
+  if (!fedAnimals) {
+    return (
+      <>
+        <section className="message-contaienr">
+          <img
+            className="avatar"
+            src="/images/happyAnimal.svg"
+            alt="A happy sloth with hands up"
+          />
+          <p className="message">Inga omatade djur hittades!</p>
+        </section>
+      </>
+    );
+  }
+
+  return (
+    <>{<AnimalsPresentation animals={fedAnimals}></AnimalsPresentation>}</>
+  );
+};
