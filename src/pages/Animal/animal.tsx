17e041ae38d4b9ce1991969cd42bfed822e14fac
tsemitris <94851943+tsemitris@users.noreply.github.com>
2024-08-30 14:32:19 +0200

    :sparkles: Add a feature for how offen the animals will be fed

--- src/pages/Animal/animal.tsx
+++ src/pages/Animal/animal.tsx
@@ -1,10 +1,47 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { IAnimalExt } from "../../models/IAnimalExt";
 import { handleImageNotFound } from "../../utils/imageHandlers";
+import { useEffect, useState } from "react";
+import { fedAnimal } from "../../utils/fedAnimal";
+import { timeNow } from "../../utils/timeNow";
 import { formatDate } from "../../utils/formatDate";
+import { timePassChecker } from "../../utils/timePassChecker";
 
 export const Animal = () => {
+  window.scrollTo(0, 0);
+
   const animal = useLoaderData() as IAnimalExt;
+
+  const [lastFed, setLastFed] = useState(animal.lastFed);
+  const [active, setActive] = useState(animal.isFed);
+
+  useEffect(() => {
+    const updateActiveState = async () => {
+      if (timePassChecker(lastFed)) {
+        setActive(false);
+
+        const storedAnimals = JSON.parse(
+          sessionStorage.getItem("animals") || "[]"
+        );
+        const updatedAnimals = storedAnimals.map((storedAnimal: IAnimalExt) =>
+          storedAnimal.id === animal.id
+            ? { ...storedAnimal, isFed: false }
+            : storedAnimal
+        );
+        sessionStorage.setItem("animals", JSON.stringify(updatedAnimals));
+      }
+    };
+
+    const interval = setInterval(updateActiveState, 1 * 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  const fedAnimalEvent = () => {
+    fedAnimal(animal.id);
+    setLastFed(timeNow());
+    setActive(true);
+  };
 
   return (
     <>
@@ -38,10 +75,13 @@ export const Animal = () => {
         <div className="animal-fed-info">
           <div className="animal-last-fed-splitter">
             <p className="fed-title">Senast matad</p>
-            <p className="fed-data">{formatDate(animal.lastFed)}</p>
+            <p className="fed-data">{formatDate(lastFed)}</p>
           </div>
           <div className="animal-last-fed-splitter fed-animal-btn-container">
-            <button className="global-btn global-btn-inactive">
+            <button
+              onClick={fedAnimalEvent}
+              className={`global-btn ${active ? "global-btn-inactive" : ""}`}
+            >
               Mata djur
             </button>
           </div>
