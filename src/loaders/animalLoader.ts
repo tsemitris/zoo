8c02869066a5177e9101ef16e23a83bdaa0b8034
tsemitris <94851943+tsemitris@users.noreply.github.com>
2024-08-30 14:36:33 +0200

    :recycle: Refactored animalLoader to use getStoredAnimals utility function

--- src/loaders/animalLoader.ts
+++ src/loaders/animalLoader.ts
@@ -1,18 +1,17 @@
 import { IAnimalExt } from "../models/IAnimalExt";
-import { getAnimals } from "../services/animalsService";
 import { IAnimalLoader } from "../models/IAnimalLoader";
+import { getStoredAnimals } from "../utils/getStoredAnimals";
 
 export const animalLoader = async ({
   params,
 }: IAnimalLoader): Promise<IAnimalExt | null> => {
-  const storedAnimals = sessionStorage.getItem("animals");
+  const animals = await getStoredAnimals();
 
-  if (!storedAnimals) {
-    await getAnimals();
-    return animalLoader({ params });
+  if (!animals) {
+    console.error("No animals found.");
+    return null;
   }
 
-  const animals: IAnimalExt[] = JSON.parse(storedAnimals);
   const animalId = params.id ? parseInt(params.id, 10) : null;
 
   if (animalId === null || isNaN(animalId)) {
