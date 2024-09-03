f0ed68aa0a7ab6f3bfb8464591501f99eac44334
tsemitris <94851943+tsemitris@users.noreply.github.com>
2024-09-03 13:33:14 +0200

    :art: Fixe asset links by adding leading dot to path

--- src/utils/imageHandlers.ts
+++ src/utils/imageHandlers.ts
@@ -1,5 +1,5 @@
 export const handleImageNotFound = (
   e: React.SyntheticEvent<HTMLImageElement, Event>
 ) => {
-  e.currentTarget.src = "/images/image-not-found.jpg";
+  e.currentTarget.src = "./images/image-not-found.jpg";
 };
